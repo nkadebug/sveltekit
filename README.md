829dd2899ff5e02f488f1ca9b722fd95e1f0b250
null <null>
2023-10-28 09:43:56 +0530

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# SvelteKit App
+Website using sveltekit
+
+## Steps Followed
+- Ran command to create app `pnpm create svelte`
+- Created configuration file `app.config.ts` 
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { appConfig } from '$lib/app.config';
+</script>
+
+<h1>Welcome to {appConfig.title}</h1>
+<p>This website is made using SvelteKit.</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation.</p>
